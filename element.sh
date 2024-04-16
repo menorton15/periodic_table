239a91bf103a99685511c89c8da70e5a6a41799c
menorton15 <menorton15@hotmail.com>
2024-04-16 22:53:45 +0000

    feat: display element details

--- element.sh
+++ element.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
@@ -11,5 +13,8 @@ else
     ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1'")
   fi
 
-  echo "$ELEMENT_DATA" 
+  echo "$ELEMENT_DATA" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
 fi
