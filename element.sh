b1cff8acd12ac1e318d8fcf586f305c48438727e
menorton15 <menorton15@hotmail.com>
2024-04-16 22:45:42 +0000

    feat: add select element query

--- element.sh
+++ element.sh
@@ -5,8 +5,11 @@ then
   echo "Please provide an element as an argument."
 else
   if [[ $1 =~ ^[0-9]+$ ]]
-    # Query using number
+  then
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $1")
   else
-    # Query using symbol/name
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1'")
   fi
+
+  echo "$ELEMENT_DATA" 
 fi
